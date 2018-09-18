2a58536654bc154c9cc44e6f1288d06dfce15c16
Nekrashevich, Natalya <n.nekrashevich@a1qa.com>
2018-09-18 21:30:03 +0300

    Task done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if(currency < 0) return {};
+    if(currency > 10000) return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    coins = {};
+    let temp = ~~ (currency / 50);
+    if(temp > 0){
+        coins.H = temp;
+        currency -= temp * 50;
+    }
+
+    temp = ~~ (currency / 25);
+    if(temp > 0){
+        coins.Q = temp;
+        currency -= temp * 25;
+    }
+
+    temp = ~~ (currency / 10);
+    if(temp > 0){
+        coins.D = temp;
+        currency -= temp * 10;
+    }
+
+    temp = ~~ (currency / 5); 
+    if(temp > 0){
+        coins.N = temp;
+        currency -= temp * 5;
+    }
+    if(currency>0){
+        coins.P = currency;
+    }
+    return coins;
+
 }
